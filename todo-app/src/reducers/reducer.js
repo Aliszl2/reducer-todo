ec04cd48dadf938b0d8ed66df4a11fa4116cfdac
Alison Law <alisparklaw@gmail.com>
2020-02-17 11:48:38 +0000

    reducer function framework

---
+++ todo-app/src/reducers/reducer.js
@@ -0,0 +1,21 @@
+import React, { useReducer } from "react";
+
+export const initialState = {
+  todoList: [
+    { item: "Paint shed", completed: false, id:1},
+    { item: "clean kitchen", completed: false, id:2}
+  ]
+};
+
+export const INPUT_CHANGE = "INPUT_CHANGE";
+export const SUBMIT_TODO = "SUBMIT_TODO";
+export const TOGGLE_COMPLETE = "TOGGLE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
+
+
+export function reducer(state, action) {
+  
+    default:
+      return state;
+}
+
